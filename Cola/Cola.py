327f885df4272573e25276d86c9356166b888270
Inge-Cesar <cesardev2913@gmail.com>
2025-04-23 20:20:26 -0400

    desencolar, ver frente

--- Cola/Cola.py
+++ Cola/Cola.py
@@ -2,8 +2,7 @@ class Nodo:
     def __init__(self, valor):
         self.valor = valor
         self.siguiente = None
-        
-   def esta_vacia(self):
+    def esta_vacia(self):
         return self.frente is None
 
     def encolar(self, valor):
@@ -15,4 +14,20 @@ class Nodo:
             self.final.siguiente = nuevo_nodo
             self.final = nuevo_nodo
         self.longitud += 1 
+
+    def desencolar(self):
+        if self.esta_vacia():
+            raise Exception("No se puede desencolar de una cola vacía")
+        valor_desencolado = self.frente.valor
+        self.frente = self.frente.siguiente
+        if self.frente is None:
+            self.final = None
+        self.longitud -= 1
+        return valor_desencolado
+
+    def ver_frente(self):
+        if self.esta_vacia():
+            print("Error: Cola vacía")
+            return None
+        return self.frente.valor
         
