1faa4e2169c416627382f096ff7dbc7818a5ace9
¨Alexis¨ <diegoalexis355@gmail.com>
2025-04-23 20:17:05 -0400

    vacio y encolar

--- Cola/Cola.py
+++ Cola/Cola.py
@@ -2,3 +2,17 @@ class Nodo:
     def __init__(self, valor):
         self.valor = valor
         self.siguiente = None
+        
+   def esta_vacia(self):
+        return self.frente is None
+
+    def encolar(self, valor):
+        nuevo_nodo = Nodo(valor)
+        if self.esta_vacia():
+            self.frente = nuevo_nodo
+            self.final = nuevo_nodo
+        else:
+            self.final.siguiente = nuevo_nodo
+            self.final = nuevo_nodo
+        self.longitud += 1 
+        
